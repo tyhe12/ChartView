a177b98918aa37420a000f6b052f14afc063efae
Tianyi He <htycm1@gmail.com>
2020-05-01 21:28:19 -0700

    adjust height

--- Sources/SwiftUICharts/LineChart/LineView.swift
+++ Sources/SwiftUICharts/LineChart/LineView.swift
@@ -82,19 +82,19 @@ public struct LineView: View {
                             self.showLegend = false
                         }
                     }
-                    .frame(width: geometry.frame(in: .local).size.width, height: 240)
+                    .frame(width: geometry.frame(in: .local).size.width, height: 300)
                     .offset(x: 0, y: 40 )
                     MagnifierRect(currentNumber: self.$currentDataNumber, valueSpecifier: self.valueSpecifier)
                         .opacity(self.opacity)
                         .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2, y: 36)
                 }
-                .frame(width: geometry.frame(in: .local).size.width, height: 240)
+                .frame(width: geometry.frame(in: .local).size.width, height: 300)
                 .gesture(DragGesture()
                 .onChanged({ value in
                     self.dragLocation = value.location
                     self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
                     self.opacity = 1
-                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
+                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 300)
                     self.hideHorizontalLines = true
                 })
                     .onEnded({ value in
